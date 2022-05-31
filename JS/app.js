4cbdd2762d3fbd65f050eb0f1f2400661e4117cf
Milos Milinkov <milos300399@gmail.com>
2022-05-31 23:54:14 +0200

    Project done
    Will come back when i learn more and how to code better

--- JS/app.js
+++ JS/app.js
@@ -1,25 +1,101 @@
 const choice = ["rock", "paper", "scissor"];
+const numberSources = ["./IMAGES/0.png", "./IMAGES/1.png", "./IMAGES/2.png", "./IMAGES/3.png", "./IMAGES/4.png", "./IMAGES/5.png", ];
+let pIcon = document.querySelector(".mLeft");
+let eIcon = document.querySelector(".mRight");
+const pScore = document.querySelector(".nP");
+const eScore = document.querySelector(".nE");
+const rButton = document.querySelector(".restartBtn");
+const buttons = document.querySelectorAll("button");
+const eMessage = document.querySelector(".endMessage");
+const endContainer = document.getElementById("endContainer");
+const pImg = document.createElement("img");
+const eImg = document.createElement("img");
+eImg.setAttribute("height", "auto");
+eImg.setAttribute("width", "150px");
+pImg.setAttribute("height", "auto");
+pImg.setAttribute("width", "150px");
+let i = 0;
+let pCounter = 1;
+let eCounter = 1;
+let ePoints = 0;
+let pPoints = 0;
+let gameOver = 0;
 
 
-function computerPlay(choice) {
-    let possibleChoice = choice[Math.floor(Math.random() * choice.length)];
-    return possibleChoice
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+
+
+        playerPlay(button.id);
+        let eChocie = enemyPlay(choice);
+        let playRoundRes = playRound(button.id, eChocie);
+        if (playRoundRes == 0) {} else if (playRoundRes == 1) {
+            for (j = pCounter; j <= 5; j++) {
+                pScore.src = numberSources[j];
+                break;
+            }
+            pCounter++;
+            pPoints++;
+        } else if (playRoundRes == 2) {
+            for (j = eCounter; j <= 5; j++) {
+                eScore.src = numberSources[j];
+                break;
+            }
+            eCounter++;
+            ePoints++;
+        }
+        if (ePoints == 5 || pPoints == 5) {
+            checkWinner(pPoints, ePoints);
+        }
+
+    })
+})
+
+function checkWinner(pPoints, ePoints) {
+    if (pPoints > ePoints) {
+        endContainer.style.visibility = "visible";
+        eMessage.textContent = "Player wins!"
+        rButton.addEventListener("click", reload, false);
+    } else if (ePoints > pPoints) {
+        endContainer.style.visibility = "visible";
+        eMessage.textContent = "Enemy wins!"
+        rButton.addEventListener("click", reload, false);
+    } else {
+        alert("Well thats a bit dumb,its a tie.....");
+    }
 }
 
-function playerPlay() {
-    let playerChoice
-    do {
-        playerChoice = prompt("Enter your move:").toLowerCase();
-        if (!choice.includes(playerChoice)) {
-            alert("Incorrect,please change your input!");
+function enemyPlay(choice) {
+    let possibleChoice = choice[Math.floor(Math.random() * choice.length)];
+    if (choice.includes(possibleChoice)) {
+        if (possibleChoice == "rock") {
+            eImg.src = "./IMAGES/rock.jpg"
+        } else if (possibleChoice == "paper") {
+            eImg.src = "./IMAGES/paper.png"
+        } else if (possibleChoice == "scissor") {
+            eImg.src = "./IMAGES/scissor.png"
         }
+        eIcon.appendChild(eImg);
+        return possibleChoice;
+    } else {
+        alert("Some error occurred,pls reload the page!")
     }
-    while (!choice.includes(playerChoice));
-    return playerChoice;
+}
+
+function playerPlay(buttonid) {
+    if (choice.includes(buttonid)) {
+        if (buttonid == "rock") {
+            pImg.src = "./IMAGES/rock.jpg"
+        } else if (buttonid == "paper") {
+            pImg.src = "./IMAGES/paper.png"
+        } else if (buttonid == "scissor") {
+            pImg.src = "./IMAGES/scissor.png"
+        }
+        pIcon.appendChild(pImg);
+    }
 }
 
 function playRound(pChoice, cChoice) {
-
     let resultFlag = -1;
     if (pChoice == cChoice) {
         resultFlag = 0;
@@ -45,31 +121,12 @@ function playRound(pChoice, cChoice) {
     return resultFlag;
 }
 
-function PlayGame() {
-    let cPoints = 0;
-    let pPoints = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = playerPlay();
-        let computerChoice = computerPlay(choice);
-        let playRoundRes = playRound(playerChoice, computerChoice);
-        if (playRoundRes == 0) {
-            alert("Uhh tiee...")
-        } else if (playRoundRes == 1) {
-            alert("Player wins the round!");
-            pPoints++;
-        } else if (playRoundRes == 2) {
-            alert("Computer wins the round!");
-            cPoints++;
-        } else if (playRoundRes == -1) {
-            alert("Some weird error happend,hmmm,gonna break....");
-        }
-    }
-    if (pPoints > cPoints) {
-        alert("Player is the winner!");
-    } else if (cPoints > pPoints) {
-        alert("Computer is the winner!");
-    } else {
-        alert("Well thats a bit dumb,its a tie.....");
-    }
+function reload() {
+    i = 0;
+    pCounter = 1;
+    eCounter = 1;
+    ePoints = 0;
+    pPoints = 0;
+    gameOver = 0;
+    reload = location.reload();
 }
-PlayGame();
